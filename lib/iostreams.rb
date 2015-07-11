ebfb3090e1de578aa0691c644153fca88c5e0c86
Reid Morrison <reidmo@gmail.com>
2015-07-11 17:30:53 -0400

    Move file

---
+++ lib/iostreams.rb
@@ -0,0 +1,9 @@
+autoload :IOStreams, 'io_streams/io_streams'
+module IOStreams
+  autoload :FileReader, 'io_streams/file_reader'
+  autoload :FileWriter, 'io_streams/file_writer'
+  autoload :GzipReader, 'io_streams/gzip_reader'
+  autoload :GzipWriter, 'io_streams/gzip_writer'
+  autoload :ZipReader,  'io_streams/zip_reader'
+  autoload :ZipWriter,  'io_streams/zip_writer'
+end
