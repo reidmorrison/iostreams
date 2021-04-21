1b03c8aaa28948f2de17b31a69f347a27744f30e
Sudheer <sudheer.meka@experian.com>
2021-04-21 15:30:42 +0530

    Jruby Unzip and Parse CSV file

--- lib/io_streams/zip/reader.rb
+++ lib/io_streams/zip/reader.rb
@@ -19,14 +19,18 @@ module IOStreams
       #   end
       if defined?(JRuby)
         # Java has built-in support for Zip files
-        def self.file(file_name, entry_file_name: nil)
-          fin = Java::JavaIo::FileInputStream.new(file_name)
-          zin = Java::JavaUtilZip::ZipInputStream.new(fin)
+        def self.file(file_name, entry_file_name: nil, &block)
+          if entry_file_name.include?(".CSV")
+            get_file_io(file_name, entry_file_name, &block)
+          else
+            fin = Java::JavaIo::FileInputStream.new(file_name)
+            zin = Java::JavaUtilZip::ZipInputStream.new(fin)
 
-          get_entry(zin, entry_file_name) ||
-            raise(Java::JavaUtilZip::ZipException, "File #{entry_file_name} not found within zip file.")
+            get_entry(zin, entry_file_name) ||
+              raise(Java::JavaUtilZip::ZipException, "File #{entry_file_name} not found within zip file.")
 
-          yield(zin.to_io)
+            yield(zin.to_io)
+          end
         ensure
           zin&.close
           fin&.close
@@ -48,20 +52,24 @@ module IOStreams
         # The input stream from the first file found in the zip file is passed
         # to the supplied block
         def self.file(file_name, entry_file_name: nil, &block)
-          Utils.load_soft_dependency("rubyzip v1.x", "Read Zip", "zip") unless defined?(::Zip)
+          get_file_io(file_name, entry_file_name, &block)
+        end
+      end
 
-          ::Zip::File.open(file_name) do |zip_file|
-            if entry_file_name
-              zip_file.get_input_stream(entry_file_name, &block)
-            else
-              result = nil
-              # Return the first file
-              zip_file.each do |entry|
-                result = entry.get_input_stream(&block)
-                break
-              end
-              result
+      def self.get_file_io(file_name, entry_file_name, &block)
+        Utils.load_soft_dependency("rubyzip v1.x", "Read Zip", "zip") unless defined?(::Zip)
+
+        ::Zip::File.open(file_name) do |zip_file|
+          if entry_file_name
+            zip_file.get_input_stream(entry_file_name, &block)
+          else
+            result = nil
+            # Return the first file
+            zip_file.each do |entry|
+              result = entry.get_input_stream(&block)
+              break
             end
+            result
           end
         end
       end
